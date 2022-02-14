1b86c49beefe61bd45745a8ceb3e2e13f8408f4a
Ayman Zaki <ayman.zaki@spectro.net>
2022-02-14 22:01:32 +0200

    initalize project

---
+++ src/main/java/com/jumia/jpay/jpayvalidator/JpayValidatorApplication.java
@@ -0,0 +1,13 @@
+package com.jumia.jpay.jpayvalidator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpayValidatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpayValidatorApplication.class, args);
+	}
+
+}
